a577b06a419c8b0c6c0a4228235eab99e935a7fd
Rachele Morino <rmorino@azavea.com>
2022-03-11 15:11:04 -0500

    WIP Week 2 Problem Set 2 03-11-22

---
+++ cs-50/week-2/substitution.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h> 
+#include <string.h>
+#include <cs50.h>
+
+int main(int argc, string argv[])
+{
+    // EDGE CASES:
+    // check to make sure only one arg
+        // if not return Usage: ./substitution key
+    // else
+    string key = argv[1];
+    if(strlen(key) != 26){
+        printf("Key must contain 26 characters.");
+    }
+    else {   
+        string input = get_string("plaintext: ");
+        printf("ciphertext: ");
+        for(int i=0; i<strlen(input); i++){
+            char c = input[i];
+            if(islower(c)) {
+                char index = c - 'a';
+                printf("%c", tolower(key[index]));
+            } 
+            else if (isupper(c)) {
+                char index = c - 'A';
+                printf("%c", toupper(key[index]));
+            }
+            else {
+                printf("%c", c);
+            }
+        }
+    }
+}
