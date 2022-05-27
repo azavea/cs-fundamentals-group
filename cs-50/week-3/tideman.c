dfc15c999116ce8309fa3bcf10ecafe2c2d071de
Rachele Morino <rmorino@azavea.com>
2022-05-27 16:54:52 -0400

    Finishin Tideman! (almost)
    We finished the program, but need to account for the scenario where there is not a clear candidate if everyone has only one locked pair over eachother (ie the sum for each row is the same, making the winner to first in the candidates array)

--- cs-50/week-3/tideman.c
+++ cs-50/week-3/tideman.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <time.h>
 
 // Max number of candidates
 #define MAX 9
@@ -28,11 +27,23 @@ pair pairs[MAX * (MAX - 1) / 2];
 int pair_count;
 int candidate_count;
 
+// Each pair has a winner, loser
+typedef struct
+{
+    int index;
+    int sum;
+}
+winner;
+
+winner winmargin;
+
 // Function prototypes
 bool vote(int rank, string name, int ranks[]);
 void record_preferences(int ranks[]);
 void add_pairs(void);
+void shuffle_pairs_for_fun(void);
 void sort_pairs(void);
+bool check_if_cycle(int og, int loser);
 void lock_pairs(void);
 void print_winner(void);
 
@@ -95,17 +106,33 @@ int main(int argc, string argv[])
     add_pairs();
     printf("og pairs \n");
     for(int i =0; i<pair_count; i++ ){
-        printf("winner: %d \n", pairs[i].winner);
-        printf("loser: %d \n", pairs[i].loser);
+        printf("winner: %d ; ", pairs[i].winner);
+        printf("loser: %d ; ", pairs[i].loser);
+        printf("lead: %d \n", preferences[pairs[i].winner][pairs[i].loser]);
     }
+    shuffle_pairs_for_fun();
     printf("shuf pairs \n");
     for(int i =0; i<pair_count; i++ ){
-        printf("winner: %d \n", pairs[i].winner);
-        printf("loser: %d \n", pairs[i].loser);
+        printf("winner: %d ; ", pairs[i].winner);
+        printf("loser: %d ; ", pairs[i].loser);
+        printf("lead: %d \n", preferences[pairs[i].winner][pairs[i].loser]);
     }
     sort_pairs();
-    // lock_pairs();
-    // print_winner();
+    printf("sorted pairs \n");
+    for(int i =0; i<pair_count; i++ ){
+        printf("winner: %d ; ", pairs[i].winner);
+        printf("loser: %d ; ", pairs[i].loser);
+        printf("lead: %d \n", preferences[pairs[i].winner][pairs[i].loser]);
+    }
+    lock_pairs();
+    printf("locked pairs \n");
+    for(int i =0; i<candidate_count; i++ ){
+        for(int j =0; j<candidate_count; j++ ){
+        printf("%d ",locked[i][j]);
+        }
+        printf("\n");
+    }
+    print_winner();
     return 0;
 }
 
@@ -175,6 +202,17 @@ void add_pairs(void)
     return;
 }
 
+void shuffle_pairs_for_fun(void)
+{
+    for(int i=0; i<pair_count; i++){
+        int random_index = (int) arc4random_uniform(i + 1);
+        pair original_pair_at_index = pairs[random_index];
+        pairs[random_index] = pairs[i];
+        pairs[i] = original_pair_at_index;
+    }
+    return;
+}
+
 // Sort pairs in decreasing order by strength of victory
 void sort_pairs(void)
 {
@@ -196,22 +234,57 @@ void sort_pairs(void)
     return;
 }
 
+bool check_if_cycle(int og, int loser)
+{
+    bool return_value = false;
+    if (loser == og){
+        printf("a cycle!");
+        return true;
+    }
+    for( int j = 0; j < candidate_count; j++){
+        if(locked[loser][j] == true){
+            return_value = check_if_cycle(og, j);
+        }
+    }
+    return return_value;
+}
+
 // Lock pairs into the candidate graph in order, without creating cycles
 void lock_pairs(void)
 {
     // TODO
-    // create a 2d array of size candidates x candidates
-    // populated with null (maybe it's already null in C?)
+    // create a 2d array of size candidates x candidates - done
+    // locked[i][j] means i is locked in over j
+    // populated with null (maybe it's already null in C?) - done
     // lock = candidate @ lock_pairs[row] --> candidate @ lock_pairs[column]
     // if there's a lock (row is winner over column) then populate with 1
     // UNLESS the rest of the column elements are already popualted with 0 because that's a cycle!
-    // at the end the greatest sum of the row for a candidate is the winner!
+
+    for (int k = 0; k < pair_count; k++){
+        bool cycle = check_if_cycle(pairs[k].winner, pairs[k].loser);
+        if(!check_if_cycle(pairs[k].winner, pairs[k].loser)){
+            locked[pairs[k].winner][pairs[k].loser] = true;
+        }
+    }
     return;
 }
 
 // Print the winner of the election
 void print_winner(void)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++){
+        int sum = 0;
+        for (int j =0; j <candidate_count; j++){
+            if(locked[i][j] == true){
+                sum += 1;
+            }
+        }
+        if (sum > winmargin.sum){
+            winmargin.index = i;
+            winmargin.sum = sum;
+        }
+    }
+
+    printf("The winner: %s!\nThe sum of locked edges: %d\n", candidates[winmargin.index], winmargin.sum);
     return;
 }
